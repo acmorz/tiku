8dbca598529ae69dd883a0678892e2812b5c4f88
15375005877 <3743994a>
2025-04-02 21:46:07 +0800

    tiku初始化项目

---
+++ src/main/java/com/fsh/tiku/annotation/AuthCheck.java
@@ -0,0 +1,26 @@
+package com.fsh.tiku.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * 权限校验 限制某个接口只能由哪个用户使用
+ *
+*
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AuthCheck {
+
+    /**
+     * 必须有某个角色
+     *
+     * @return
+     */
+    String mustRole() default "";
+
+}
+
